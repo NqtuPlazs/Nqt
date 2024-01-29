e03296961c69f92d478a1887e46242ef6da78c68
NqtuPlazs <158069518+NqtuPlazs@users.noreply.github.com>
2024-01-29 21:04:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Welcoming In Elite Slayers","Listening to Nqtu","Elite Slayers on Top"];
 
 
 let currentIndex = 0;
